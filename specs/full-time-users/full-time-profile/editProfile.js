b32d10e33be5021add901150a966e2d07a1250d1
Selina <thanhhuong290897@gmail.com>
2022-08-22 23:22:50 +0700

    update code for full time profile

---
+++ specs/full-time-users/full-time-profile/editProfile.js
@@ -0,0 +1,51 @@
+import { By } from 'selenium-webdriver';
+import { expect } from 'chai';
+
+import { baseUrl } from '../../../utils/config';
+import { buildDriver } from '../../../utils/browser';
+import { doAuthentication } from '../../../supports/login';
+
+describe('full-time-users/full-time-profile/editProfile', () => {
+    let driver;
+
+    before(async () => {
+        driver = await buildDriver();
+        await doAuthentication(driver, 'ftu');
+    });
+
+    after(async () => {
+        await driver.quit();
+    });
+
+    context('success', async () => {
+        const updatedFirstName = 'New First Name';
+
+        it('updates user information', async () => {
+            await driver.get(`${baseUrl}/users_panel/profiles/edit`);
+            await driver.findElement(By.css('#user_first_name')).clear();
+            await driver.findElement(By.css('#user_first_name')).sendKeys(updatedFirstName);
+            await driver.findElement(By.css('form .btn-update')).click();
+
+            //await driver.get(`${baseUrl}/users_panel/profiles`);
+
+            const profileText = await driver.findElement(By.css('.user-profiles')).getText();
+            expect(profileText).to.contain(updatedFirstName);
+        });
+    });
+    // Case fail
+    //   context('failure', async () => {
+    //     const updatedFirstName = 'New First Name';
+
+    //     it('updates user information', async () => {
+    //       await driver.get(`${baseUrl}/users_panel/profiles/edit`);
+    //       await driver.findElement(By.css('#user_first_name')).clear()
+    //       await driver.findElement(By.css('#user_first_name')).sendKeys(updatedFirstName);
+    //       await driver.findElement(By.css('form .btn-update')).click();
+
+    //       await driver.get(`${baseUrl}/users_panel/profiles`);
+
+    //       const profileText = await driver.findElement(By.css('.user-profiles')).getText();
+    //       expect(profileText).to.contain(updatedFirstName);
+    //     });
+    //   });
+});
