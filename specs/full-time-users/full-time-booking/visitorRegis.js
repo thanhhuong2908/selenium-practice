01a3c79087f26c4d977585688a9140bf880c6e94
Selina <thanhhuong290897@gmail.com>
2022-08-25 09:14:31 +0700

    update case fail for visitor registration.

--- specs/full-time-users/full-time-booking/visitorRegis.js
+++ specs/full-time-users/full-time-booking/visitorRegis.js
@@ -1,4 +1,4 @@
-import { By } from 'selenium-webdriver';
+import { By, Actions } from 'selenium-webdriver';
 import { expect } from 'chai';
 
 import { baseUrl } from '../../../utils/config';
@@ -18,48 +18,59 @@ describe('full-time-users/full-time-booking/visitorRegis', () => {
   });
 
   // context('success', async () => {
-  //   const dateArrival = '23/08/2022';
+  //   const dateArrival = '25/08/2022';
   //   const timeArrival = '9:00am';
   //   const endTimeArrival = '10:00am';
   //   const visitorName = 'New';
   //   const lastName = 'Guest';
   //   const flashNotice = 'Visitors have been created successfully.';
 
-  //   it('add visitor success', async () => {
-  //     await driver.get(`${baseUrl}/users_panel/visitors`);
-  //     await driver.findElement(By.css('#new-visitor-modal')).click();
-  //     await driver.findElement(By.css('#arrival_date')).senKey(dateArrival);
-  //     await driver.findElement(By.css('#arrival_time')).senKey(timeArrival);
-  //     await driver.findElement(By.css('#end_time')).senKey(endTimeArrival);
-  //     await driver.findElement(By.css('#visitor_name')).senKey(visitorName);
-  //     await driver.findElement(By.css('#visitor_name')).senKey(lastName);
+  //   it.only('add visitor success', async () => {
+  //     await driver.findElement(By.css('a[href*="users_panel/visitors"]')).click();
+  //     await driver.findElement(By.css('button[data-target="#new-visitor-modal"]')).click();
   //     await driver.sleep(3000);
-  //     await driver.findElement(By.css('.create-button')).click();
+  //     await driver.findElement(By.css('#arrival_date')).click();
+  //     await driver.sleep(200);
+  //     const a = await driver.findElement(By.css('td.undefined > a'));
+  //     await driver.actions().click(a).perform();
+    
+      
+  //     // console.log(await a.getAttribute('outerHTML'));
+  //     // await a.click(); 
+  //     await driver.sleep(3000);
+  //     await driver.findElement(By.css('#arrival_time')).click();
+  //     // await driver.findElement(By.css('#end_time')).sendKeys(endTimeArrival);
+  //     // await driver.findElement(By.css('#visitor_name')).sendKeys(visitorName);
+  //     // await driver.findElement(By.css('#visitor_name')).sendKeys(lastName);
+  //     // await driver.sleep(3000);
+  //     // await driver.findElement(By.css('.create-button')).click();
 
-  //     const flashText = await driver.findElement(By.css('#flash_notice')).getText();
-  //     expect(flashText).to.contain(flashNotice);
+  //     // const flashText = await driver.findElement(By.css('#flash_notice')).getText();
+  //     // expect(flashText).to.contain(flashNotice);
   //   });
   // });
 
   context('fail', async () => {
-    const dateArrival = '23/08/2022';
+    const dateArrival = '25/08/2022';
     const timeArrival = '9:00am';
     const endTimeArrival = '10:00am';
 
-    it.only('add visitor fail with first name is blank', async () => {
-      await driver.get(`${baseUrl}/users_panel/visitors`);
-      await driver.findElement(By.xpath('/html/body/div[1]/section[1]/div[3]/button')).click();
+    it('add visitor fail with first name is blank', async () => {
+      //await driver.get(`${baseUrl}/users_panel/visitors`);
+
+      await driver.findElement(By.css('a[href*="users_panel/visitors"]')).click();
+      await driver.findElement(By.css('button[data-target="#new-visitor-modal"]')).click();
       await driver.sleep(3000);
-      console.log(await nut.getAttribute('outerHTML'));
-      // await driver.findElement(By.css('#arrival_date')).sendKeys(dateArrival);
-      // await driver.findElement(By.css('#arrival_time')).sendKeys(timeArrival);
-      // await driver.findElement(By.css('#end_time')).sendKeys(endTimeArrival);
+      //console.log(await nut.getAttribute('outerHTML'));
+      await driver.findElement(By.css('#arrival_date')).sendKeys(dateArrival);
+      await driver.findElement(By.css('#arrival_time')).sendKeys(timeArrival);
+      await driver.findElement(By.css('#end_time')).sendKeys(endTimeArrival);
 
-      // await driver.sleep(3000);
-      // await driver.findElement(By.css('.create-button')).click();
+      await driver.sleep(3000);
+      await driver.findElement(By.css('.create-button')).click();
 
-      // const errorText = await driver.findElement(By.css('input:invalid')).getText();
-      //   expect(errorText).to.equal('');
+      const errorText = await driver.findElement(By.css('input:invalid')).getText();
+       expect(errorText).to.equal('');
     });
   });
 });
